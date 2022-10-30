4a341a652e77baad7ce84a4abfa3644bb3037a14
Florencia Moreno <morenoflorenciacode@gmail.com>
2022-10-29 22:55:50 -0300

    Entrega Proyecto Final React

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -2,20 +2,31 @@ import { useState, useContext } from "react"
 import { CartContext } from "../../context/CartContext"
 import { getDocs, addDoc, collection, where, query, documentId, writeBatch } from 'firebase/firestore'
 import { db } from '../../Service/firebase'
+import Swal from "sweetalert2"
+import './Checkout.css'
 
 const Checkout =  () => {
     const [loading, setLoading] = useState(false)
 
     const { cart, total, clearCart } = useContext(CartContext)
+    const [nombre, setNombre] = useState('')
+    const [apellido, setApellido] = useState('')
+    const [telefono, setTelefono] = useState('')
+    const [email, setEmail] = useState('')
+    const [email2, setEmail2] = useState('')
 
-    const createOrder = async () => {
+    const createOrder = async (e) => {
+        e.preventDefault()
         setLoading(true)
+        if (email === email2 && nombre !== "" && apellido !== "" && telefono !== "" && email !== ""){
         try {
         const objOrder = {
                  buyer: {
-                     name: 'Florencia Moreno',
-                     phone: '123456789',
-                     email: 'maflorencia.moreno@hotmail.com'
+                     nombre: `${nombre}`,
+                     apellido: `${apellido}`,
+                     email: `${email}`,
+                     email2: `${email2}`,
+                     telefono: `${telefono}`
                  },
                  items: cart,
                  total
@@ -50,26 +61,87 @@ const Checkout =  () => {
 
                 const orderRef = collection(db, 'orders')
                 const orderAdded = await addDoc(orderRef, objOrder)
-
-                console.log(`El código de su orden es: ${orderAdded.id}`)
+                Swal.fire(
+                    'Su orden se ha realizado con exito',
+                    `El código de su orden es: ${orderAdded.id}`,
+                    `success`
+                )
                 clearCart()
             } else {
-                console.log('Producto sin stock')
+                Swal.fire(
+                    'Producto sin stock',
+                    '',
+                    'error'
+                )
             }
         } catch (error) {
             console.log(error)
         } finally {
             setLoading(false)
         }
+    }else {
+        Swal.fire(
+            'Para continuar, debes completar todos los datos',
+            '',
+            'error'
+        )
+        setLoading(false)
     }
+}
 
     if(loading) {
-        return <h1>Su orden se esta generando...</h1>
+        return  <div>
+        <h1>Su orden se esta procesando...</h1>
+           <div className='loading2'>
+             <h2>Cargando...</h2>
+           </div>
+        </div>
     }
 
     return (
         <>
-            <h1>Checkout</h1>
+            <h2>Datos de contacto</h2>
+            <form className='form'>
+            <input
+                name='nombre'
+                className='formInput' 
+                value={nombre} 
+                placeholder='Nombre' 
+                required="required"
+                onChange={(e) => setNombre(e.target.value)} />
+                <input 
+                name='apellido'
+                className='formInput' 
+                value={apellido}  
+                placeholder='Apellido' 
+                required="required"
+                onChange={(e) => setApellido(e.target.value)}/>
+                <input
+                name='email' 
+                className='formInput' 
+                value={email} 
+                placeholder='Email' 
+                type='email'
+                required="required"
+                onChange={(e) => setEmail(e.target.value)}/>
+                <input
+                name='email2'
+                className='formInput' 
+                value={email2} 
+                placeholder='Confirme Email' 
+                type='email'
+                required="required"
+                onChange={(e) => setEmail2(e.target.value)}/>
+                <input
+                name='telefono'
+                className='formInput' 
+                value={telefono} 
+                placeholder='Telefono' 
+                required="required"
+                type='number'onChange={(e) => setTelefono(e.target.value)}/>
+                <button type="submit" onClick={createOrder} className='buttonOrder' >Generar Orden</button>
+                
+            </form>
             <button onClick={createOrder}>Agregar documento</button>
 
         </>
